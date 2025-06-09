15b22d5b83acd38c9ffbe7c4e6186a043b444ab4
Vezirov <aganiyazwezirow@gmail.com>
2025-06-09 10:51:56 +0300

    İlk commit

---
+++ HomeWork_3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+class ConfigLoader
+{
+    private string _configData;
+
+    private ConfigLoader() { }
+
+    
+    public static async Task<ConfigLoader> CreateAsync(string filePath)
+    {
+        var instance = new ConfigLoader();
+        await instance.LoadConfigAsync(filePath);
+        return instance;
+    }
+
+    private async Task LoadConfigAsync(string filePath)
+    {
+        
+        _configData = await File.ReadAllTextAsync(filePath);
+    }
+
+    public string GetConfig()
+    {
+        if (_configData == null)
+            throw new InvalidOperationException("Config not loaded yet.");
+        return _configData;
+    }
+}
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            ConfigLoader loader = await ConfigLoader.CreateAsync("config.txt");
+
+            string config = loader.GetConfig();
+            Console.WriteLine(config);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
+
---
+++ HomeWork_4/HomeWork_4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var progress = new Progress<int>(percent =>
+        {
+            Console.WriteLine($"Progress: {percent}%");
+        });
+
+        await LongOperationAsync(progress);
+
+        Console.WriteLine("Operation completed!");
+    }
+
+    static async Task LongOperationAsync(IProgress<int> progress)
+    {
+        int totalSteps = 5;
+
+        for (int i = 1; i <= totalSteps; i++)
+        {
+           
+            await Task.Delay(1000); 
+            
+
+            int percent = (i * 100) / totalSteps;
+            progress.Report(percent);
+        }
+    }
+}
