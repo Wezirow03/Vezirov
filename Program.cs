9b0d790b14f9f3dc9d8e89ada6ea9f23d27a9e2e
Wezirow03 <aganiyazwezirow@gmail.com>
2025-06-12 16:37:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string result = GetPageContent("https://example.com");
+        Console.WriteLine(result);
+    }
+
+    static string GetPageContent(string url)
+    {
+        HttpClient client = new HttpClient();
+        return client.GetStringAsync(url).Result;
+    }
+
+
+
+
+  /*  static async Task Main(string[] args)
+    {
+        string result = await GetPageContentAsync("https://example.com");
+        Console.WriteLine(result);
+    }
+
+    static async Task<string> GetPageContentAsync(string url)
+    {
+        HttpClient client = new HttpClient();
+        return await client.GetStringAsync(url);
+    }*/
+}
